0733b8dd810e29e9721e70d39f8112b170e7fca9
Tian <tian.qiu142@gmail.com>
2021-02-16 12:22:13 -0800

    initiated author select in AddPostForm

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -6,11 +6,13 @@ import { postAdded } from './postsSlice'
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [author, setAuthor] = useState('')
 
   const dispatch = useDispatch()
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
+  const onAuthorChanged = (e) => setAuthor(e.target.value)
 
   const onSavePostClicked = () => dispatch(postAdded(title, content))
 
@@ -33,6 +35,10 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
+        <label>Set Author:</label>
+        <select value={author} onChange={onAuthorChanged}>
+          <option value=""></option>
+        </select>
         <button type="button" onClick={onSavePostClicked}>
           Save Post
         </button>
