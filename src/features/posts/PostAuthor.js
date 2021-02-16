b92a8e9dbb04e91a2f7526eebb36990fbf36e65b
Tian <tian.qiu142@gmail.com>
2021-02-16 12:35:27 -0800

    created PostAuthor component that takes in a userId prop, which is post.author in a post

---
+++ src/features/posts/PostAuthor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { useSelector } from 'react-redux'
+
+export const PostAuthor = ({ userId }) => {
+  const author = useSelector((state) =>
+    state.users.find((user) => user.id === userId)
+  )
+
+  return <div>{author ? author.name : 'Unknown Author'}</div>
+}
